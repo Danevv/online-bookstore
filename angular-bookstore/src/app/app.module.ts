bd8281c4025f572edda0234badaee8845f3553eb
Danevv <vasil.danev@gmail.com>
2020-06-03 14:06:29 +0200

    cart details page is done and working

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -17,8 +17,10 @@ import { SearchComponent } from './components/search/search.component';
 import { SearchNotFoundComponent } from './components/search-not-found/search-not-found.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'books/:id', component: BookDetailsComponent},
   {path: 'books', component: BookListComponent},
   {path: 'search/:keyword', component: BookListComponent},
@@ -37,6 +39,7 @@ const routes: Routes = [
     SearchNotFoundComponent,
     BookDetailsComponent,
     CartStatusComponent,
+    CartDetailsComponent,
     //JwPaginationComponent
   ],
   imports: [
